eaf957e926e83066c5450a09a1c7a509af907630
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-07-25 18:38:55 -0700

    first lesson done

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,13 +1,15 @@
 describe('home page', () => {
-  it('the h1 contains the correct text', () => {
+
+  beforeEach(()=>{
     cy.visit('http://localhost:3000')
-   
+  })
+
+  it('the h1 contains the correct text', () => {
     //I added .should("be.visible") so I can see this element on cypress
     cy.get('[data-test="hero-heading"]').should("be.visible").contains("Testing Next.js Applications with Cypress")
   })
 
-  it.only("the features on the homepage are correct", ()=>{
-    cy.visit("http://localhost:3000")
+  it("the features on the homepage are correct", ()=>{
     cy.get("dt").should("be.visible").eq(0).contains('4 Courses')
     cy.get("dt").should("be.visible").eq(1).contains("25+ Lessons")
     cy.get("dt").should("be.visible").eq(2).contains("Free and Open Source")
